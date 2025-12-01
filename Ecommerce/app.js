1c82b823c4e41103df0951ae21eb7d39a5d533a9
priya kanani <priyakanani1234@gmail.com>
2025-12-01 18:23:22 +0530

    add the file

---
+++ Ecommerce/app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: "*"  // or "https://priyakanani09.github.io" for more secure
+}));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/Img', express.static(path.join(__dirname, 'Img'), {
+  cacheControl: false,
+  etag: false,
+  maxAge: 0
+}));
+ // must be before 404
+ app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Start server
+const port = process.env.PORT || 5002;
+
+app.listen(5002, () => {
+  console.log("Server running on port", port);
+});
+
+
+module.exports = app;
