f0c093be125909acbe8a5211a002906b0d58cb43
priya kanani <priyakanani1234@gmail.com>
2026-01-27 09:12:32 +0530

    update the cart api

--- Ecommerce/controller/cartController.js
+++ Ecommerce/controller/cartController.js
@@ -1,4 +1,5 @@
 const Cart = require("../model/Cartmodel");
+const Product = require("../model/productmodel")
 
 /* ADD TO CART */
 exports.addToCart = async (req, res) => {
@@ -7,7 +8,7 @@ exports.addToCart = async (req, res) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const userId = req.user.id;
+    const userId = req.user._id;
     const { productId } = req.body;
 
     const product = await Product.findById(productId);
@@ -50,7 +51,7 @@ exports.addToCart = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Product added to cart",
-      cart,
+      items: cart.items,
     });
   } catch (error) {
     console.error("Add to cart error:", error);
@@ -62,7 +63,7 @@ exports.addToCart = async (req, res) => {
 /* GET USER CART */
 exports.getCart = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
 
     const cart = await Cart.findOne({ userId })
       .populate("items.productId");
@@ -82,7 +83,7 @@ exports.getCart = async (req, res) => {
 exports.updateQty = async (req, res) => {
   try {
     const { productId, qty } = req.body;
-    const userId = req.user.id;
+    const userId = req.user._id;
     const cart = await Cart.findOne({ userId });
 
     if (!cart) {
@@ -117,7 +118,7 @@ exports.updateQty = async (req, res) => {
 /* REMOVE ITEM */
 exports.removeItem = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
     const cart = await Cart.findOne({ userId});
 
     if (!cart) {
@@ -142,7 +143,7 @@ exports.removeItem = async (req, res) => {
 /* CLEAR CART */
 exports.clearCart = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
     await Cart.findOneAndDelete({ userId});
 
     res.json({
