0521e05528c2776464e886467ee025667232e58c
priya kanani <priyakanani1234@gmail.com>
2026-02-06 09:32:17 +0530

    add the get all order for admin

--- Ecommerce/routes/index.js
+++ Ecommerce/routes/index.js
@@ -58,7 +58,7 @@ router.get("/search",productController.searchProduct);
 // User Order 
 router.post('/order',jwtAuth,ordercontroller.order);
 router.get("/getorder",jwtAuth,ordercontroller.getorder);
-router.get("/getorder", ordercontroller.getAllOrders);
+router.get("/getallorder", ordercontroller.getAllOrders);
 
 //recently view product 
 router.post('/recently-view',jwtAuth,recentlyViewed.saveRecentlyViewed);
