670eb7bafe3ea56a443b56eade9cdc0df9149fb7
priya kanani <priyakanani1234@gmail.com>
2026-02-27 08:40:41 +0530

    update the COD page

--- e-com/src/component/CODCheckout.js
+++ e-com/src/component/CODCheckout.js
@@ -147,7 +147,7 @@ function CODCheckout() {
                 onChange={handleChange}
                 value={form.firstName}
                 className="w-full border border-gray-300 rounded px-3 py-2 text-sm
-                          focus:outline-none focus:ring-2 focus:ring-green-500"
+                     focus:outline-none focus:ring-2 focus:ring-green-500"
               />
             </div>
 
